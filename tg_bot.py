913fae37c2eaa677f713de30885ab6145fa1076b
LILJELLY123 <135241337+LILJELLY123@users.noreply.github.com>
2024-12-19 12:22:29 +0300

    json file

--- tg_bot.py
+++ tg_bot.py
@@ -3,3 +3,9 @@ import requests
 
 BOT_TOKEN = '8031786381:AAGeSljDFGo-x2q6nRTwbfgyOpIAjXfc1qA' #Мой токен
 bot = telebot.TeleBot(BOT_TOKEN)
+
+# Функция для загрузки данных из JSON-файла
+def load_matches():
+    with open('sports_events.json', 'r') as file:
+        data = json.load(file)
+        return data['matches']
